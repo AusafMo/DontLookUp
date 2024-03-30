7fbbc84fd4e9c19cc3e4b3cc83349401a91e992b
Mohammad Ausaf <75237046+AusafMo@users.noreply.github.com>
2024-03-31 00:49:30 +0530

    jwt

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from pymongo.server_api import ServerApi
 import asyncio
 import os
 from dotenv import load_dotenv
+import secrets
 load_dotenv()
 
 from pydantic import BaseModel
@@ -29,6 +30,7 @@ db = initcon()
 SECRET_KEY = os.getenv("secret_key")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+dluSeed = os.getenv("dluSeed")
 
 class User(BaseModel):
     username: str
@@ -96,15 +98,27 @@ def checkDluKey(api_key_header: str = Security(api_key_header)) -> str:
         detail="Invalid or missing DLU API Key",
     )
 
-@app.post("/protected/createDluKey/{new_key}")
-async def insert_dlu_key(new_key:str,  current_user: User = Depends(get_current_user)):
+@app.post("/protected/createDluKey/")
+async def insert_dlu_key( current_user: User = Depends(get_current_user)):
     try:
-       res =  db.keyMap.insert_one({"dlu_key": new_key, "gem_keys": [], "user": current_user.get("username")})
+       
+       dlu_key = secrets.token_urlsafe(12)
+       res =  db.keyMap.insert_one({"dlu_key": dlu_key, "gem_keys": [], "user": current_user.get("username")})
        if res.acknowledged == False:
             return {"message": "dlu_key not added"}
-       return {"message": "dlu_key added"}
+       return {"message": "dlu_key added", 'dlu_key': dlu_key}
     except Exception as e:
         return {"message": str(e)}
+    
+@app.get("/protected/getDluKeys")
+async def get_dlu_keys(current_user: User = Depends(get_current_user)):
+    keys = db.keyMap.find({"user": current_user.get("username")})
+    return {"keys": [key.get("dlu_key") for key in keys]}
+
+@app.get("/protected/getGemKeys/{dlu_key}")
+async def get_gem_keys(dlu_key: str, current_user: User = Depends(get_current_user)):
+    keys = db.keyMap.find_one({"dlu_key": dlu_key})
+    return {"keys": keys.get("gem_keys")}
 
 @app.post("/protected/createGemKey/{dlu_key}/{new_key}")
 async def insert_gem_key(dlu_key: str, new_key:str,  current_user: User = Depends(get_current_user)):
@@ -116,6 +130,7 @@ async def insert_gem_key(dlu_key: str, new_key:str,  current_user: User = Depend
         return {"message": "gem_key added"}
     except Exception as e:
         return {"message": str(e)}
+
 
 @app.get("/protected/{keyword}")
 async def query(keyword:str =None, temperature: float = .5, dlu_key: str = Security(checkDluKey), current_user: User = Depends(get_current_user) ):
